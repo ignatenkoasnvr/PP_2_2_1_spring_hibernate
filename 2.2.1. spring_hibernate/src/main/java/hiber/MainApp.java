2f6645562a101b8e4a09da170ab2f5a76831977b
ignatenkoasnvr <ignatenkoasnvr@gmail.com>
2023-06-08 20:59:15 +0300

    Colution(v1)

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -2,6 +2,7 @@ package hiber;
 
 import hiber.config.AppConfig;
 import hiber.model.User;
+import hiber.model.Car;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -19,6 +20,17 @@ public class MainApp {
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+      Car car1 = new Car("Vaz", 2109);
+      Car car2 = new Car("Gaz", 3110);
+
+      User user5 = new User("Vanya", "Ivanov", "IvanovI@gmail.com");
+      user5.setCar(car1);
+
+      User user6 = new User("Petya", "Petrov", "PetrovP@gmail.com");
+      user6.setCar(car2);
+      userService.add(user5);
+      userService.add(user6);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
