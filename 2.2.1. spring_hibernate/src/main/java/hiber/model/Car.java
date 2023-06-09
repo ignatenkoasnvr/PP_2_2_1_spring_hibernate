99f95e8a3fd6feff9f3414ac2d421ee8b4c9bf2c
ignatenkoasnvr <ignatenkoasnvr@gmail.com>
2023-06-09 11:57:17 +0300

    Solition(v2)

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -1,9 +1,10 @@
 package hiber.model;
+import javax.persistence.*;
 @Entity
 @Table(name = "cars")
 public class Car {
     @Id
-    @GeneratedValue(strategy = Generation.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column (name = "model")
