56f161f98cc5fb73e7f8bb923402242d96079c0b
ignatenkoasnvr <59367205+ignatenkoasnvr@users.noreply.github.com>
2023-06-09 16:31:21 +0300

    Update UserDaoImp.java
    10

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -4,34 +4,40 @@ import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+   private final SessionFactory sessionFactory;
 
-    @Override
-    public void add(User user) {
-        sessionFactory.getCurrentSession().save(user);
-    }
+   @Autowired
+   public UserDaoImp(SessionFactory sessionFactory) {
+      this.sessionFactory = sessionFactory;
+   }
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
-        return query.getResultList();
-    }
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
 
-    @Override
-    @SuppressWarnings("unchecked")
-    public User getUserByCar(String model, int series){
-        String hql = "form User user where user.car.model =:model and user.car.series =:series";
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
-        query.setParameter("model", model).setParameter("series", series);
-        return query.getSingleResult();
-    }
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> getListUsers() {
+      TypedQuery<User> query=sessionFactory
+              .getCurrentSession()
+              .createQuery("from User",User.class);
+      return query.getResultList();
+   }
+
+   @Override
+   public User getUser(String model, int series) {
+      return (User) sessionFactory.getCurrentSession()
+              .createQuery("from User u where u.userCar.model=:model and u.userCar.series=:series", User.class)
+              .setParameter("model", model)
+              .setParameter("series", series)
+              .uniqueResult();
+   }
+
 }
