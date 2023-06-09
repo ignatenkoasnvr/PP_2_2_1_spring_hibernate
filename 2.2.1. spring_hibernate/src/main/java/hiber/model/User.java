74932b11e16e3e5de0edbb83c63aa8e80665a617
ignatenkoasnvr <59367205+ignatenkoasnvr@users.noreply.github.com>
2023-06-09 16:30:31 +0300

    Update User.java
    7

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,80 +1,121 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    //region Fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-   @Column(name = "name")
-   private String firstName;
+    @Column(name = "name")
+    private String firstName;
 
-   @Column(name = "last_name")
-   private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
-   @Column(name = "email")
-   private String email;
+    @Column(name = "email")
+    private String email;
 
-   @OneToOne(mappedBy = "user")
-   private Car car;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "car_id")
+    private Car userCar;
+    //endregion
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    //region Constructors
+    public User() {
+    }
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-   public Long getId() {
-      return id;
-   }
+    public User(String firstName, String lastName, String email, Car userCar) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.userCar = userCar;
+    }
+    //endregion
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    //region Getters
+    public Long getId() {
+        return id;
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public Car getUserCar() {
+        return userCar;
+    }
+    //endregion
 
-   public String getEmail() {
-      return email;
-   }
+    //region Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public Car getCar() {
-      return car;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public void setCar(Car car) {
-      this.car = car;
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public User(Long id, String firstName, String lastName, String email) {
-      this.id = id;
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    public void setUserCar(Car userCar) {
+        this.userCar = userCar;
+    }
+    //endregion
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return Objects.equals(id, user.id) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(userCar, user.userCar);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, userCar);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", userCar=" + userCar +
+                '}';
+    }
 }
